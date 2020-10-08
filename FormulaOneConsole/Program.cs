122c78cd1c36b5ea4092e18c8493bdeef467e52d
Stud. Luca Castelli <l.castelli.1642@vallauri.net>
2020-10-08 11:56:29 +0200

    -a

--- FormulaOneConsole/Program.cs
+++ FormulaOneConsole/Program.cs
@@ -1,12 +1,89 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.
 
 namespace FormulaOneConsole
 {
     class Program
     {
+        static DbTools db = new DbTools();
+        public const string WORKINGPATH = @"C:\data\formulaone\";
+        private const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + WORKINGPATH + @"FormulaOne.mdf;Integrated Security=True";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char scelta = ' ';
+            do
+            {
+                Console.WriteLine("\n*** FORMULA ONE - BATCH SCRIPTS ***\n");
+                Console.WriteLine("1 - Create Countries");
+                Console.WriteLine("2 - Create Teams");
+                Console.WriteLine("3 - Create Drivers");
+                Console.WriteLine("------------------");
+                Console.WriteLine("X - EXIT\n");
+                scelta = Console.ReadKey(true).KeyChar;
+                switch (scelta)
+                {
+                    case '1':
+                        callExecuteSqlScript("Countries");
+                        break;
+                    case '2':
+                        callExecuteSqlScript("Teams");
+                        break;
+                    case '3':
+                        callExecuteSqlScript("Drivers");
+                        break;
+                    default:
+                        if (scelta != 'X' && scelta != 'x') Console.WriteLine("\nUncorrect Choice - Try Again\n");
+                        break;
+                }
+            } while (scelta != 'X' && scelta != 'x');
+        }
+        static bool callExecuteSqlScript(string scriptName)
+        {
+            try
+            {
+                db.ExecuteSqlScript(scriptName + ".sql");
+                Console.WriteLine("\nCreate " + scriptName + " - SUCCESS\n");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nCreate " + scriptName + " - ERROR: " + ex.Message + "\n");
+                return false;
+            }
+        }
+        public void ExecuteSqlScript(string sqlScriptName)
+        {
+            var fileContent = File.ReadAllText(WORKINGPATH + sqlScriptName);
+            fileContent = fileContent.Replace("\r\n", "");
+            fileContent = fileContent.Replace("\r", "");
+            fileContent = fileContent.Replace("\n", "");
+            fileContent = fileContent.Replace("\t", "");
+            var sqlqueries = fileContent.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var con = new SqlConnection(CONNECTION_STRING);
+            var cmd = new SqlCommand("query", con);
+            con.Open(); int i = 0;
+            foreach (var query in sqlqueries)
+            {
+                cmd.CommandText = query; i++;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException err)
+                {
+                    Console.WriteLine("Errore in esecuzione della query numero: " + i);
+                    Console.WriteLine("\tErrore SQL: " + err.Number + " - " + err.Message);
+                }
+            }
+            con.Close();
         }
     }
+   
 }
