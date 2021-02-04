84ea42a47459b27a03ec873ce7192fd7f3f992ff
CastelliLuca <58586845+CastelliLuca@users.noreply.github.com>
2021-02-04 12:35:01 +0100

    add FormulaOneWebServices

---
+++ CountryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FormulaOneDLL;
+
+namespace FormulaOneWebServices
+{
+    [Route("api/country")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        // GET: api/Country
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            DBtools d = new DBtools();
+            return d.getCountries();
+        }
+
+        // GET: api/Country/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Country
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Country/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
